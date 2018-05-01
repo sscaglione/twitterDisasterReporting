892f869b22b2e936d376ea30e12089e7f7c948fe
Samantha Scaglione <sscaglio@nd.edu>
2018-05-01 10:25:58 -0400

    append tweets if sample txt file is out of order

--- append_tweets_realtime.py
+++ append_tweets_realtime.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, time, timedelta
 
 import string
 import time
@@ -11,9 +11,9 @@ end_time = datetime.strptime(end_time_s, '%d %b %Y %H:%M:%S')
 with open("sandy_sample.txt") as tweets_file:
 	while (end_time > start_time):
 		for line in tweets_file:
-			s = line.split("created_at")[1][9:29]
-			d = datetime.strptime(s, '%d %b %Y %H:%M:%S')
-			if (d <= start_time):
+			tweet_time_s = line.split("created_at")[1][9:29]
+			tweet_time = datetime.strptime(tweet_time_s, '%d %b %Y %H:%M:%S')
+			if (tweet_time == start_time):
 				print(line)
 		time.sleep(1)
-		start_time += datetime.timedelta(0,1)
+		start_time += timedelta(seconds=1)
