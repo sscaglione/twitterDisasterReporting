7765ea679f4675ed4bd104393cb2f3e520b2da51
Samantha Scaglione <sscaglio@nd.edu>
2018-05-01 19:52:43 -0400

    stream_tweets.py

---
+++ stream_tweets.py
@@ -0,0 +1,22 @@
+from datetime import datetime, date, time, timedelta
+
+import string
+import time
+
+start_time_s = "27 Oct 2012 13:36:08"
+start_time = datetime.strptime(start_time_s, '%d %b %Y %H:%M:%S')
+
+output_file = open("sample.txt", "w+", 0)
+
+with open("sandy_sample.txt") as tweets_file:
+	for line in tweets_file:
+		tweet_time_s = line.split("created_at")[1][9:29]
+		tweet_time = datetime.strptime(tweet_time_s, '%d %b %Y %H:%M:%S')
+		if (tweet_time <= start_time):
+			output_file.write(line)
+		elif (d > start_time):
+			td = d - start_time #td = timedelta
+			time.sleep(td.total_seconds())
+			start_time += td
+			output_file.write(line)
+
