6dfb0b248533c243a01ada2ffe2630aca96110ce
Samantha Scaglione <sscaglio@nd.edu>
2018-05-01 10:16:09 -0400

    append_tweets_realtime.py

--- append_tweets_realtime.py
+++ append_tweets_realtime.py
@@ -3,17 +3,17 @@ from datetime import datetime
 import string
 import time
 
-start_time_s = "27 Oct 2012 13:36:22"
+start_time_s = "27 Oct 2012 13:36:08"
 start_time = datetime.strptime(start_time_s, '%d %b %Y %H:%M:%S')
+end_time_s = "27 Oct 2012 13:37:08"
+end_time = datetime.strptime(end_time_s, '%d %b %Y %H:%M:%S')
 
 with open("sandy_sample.txt") as tweets_file:
-	for line in tweets_file:
-		s = line.split("created_at")[1][9:29]
-		d = datetime.strptime(s, '%d %b %Y %H:%M:%S')
-		if (d <= start_time):
-			print(line)
-		elif (d > start_time):
-			td = d - start_time #td = timedelta
-			time.sleep(td.total_seconds())
-			start_time += td
-			print(line)
+	while (end_time > start_time):
+		for line in tweets_file:
+			s = line.split("created_at")[1][9:29]
+			d = datetime.strptime(s, '%d %b %Y %H:%M:%S')
+			if (d <= start_time):
+				print(line)
+		time.sleep(1)
+		start_time += datetime.timedelta(0,1)
