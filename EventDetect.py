797a358706433dbf2060bda68388a36a8ad3254c
Mara <marastaines@gmail.com>
2018-05-01 21:32:59 -0400

    event detect update

--- EventDetect.py
+++ EventDetect.py
@@ -1,12 +1,15 @@
 import datetime as dt
 import sys
-import time
+from time import sleep
 from sklearn.externals import joblib
 from tweet_to_json import tweet_to_json
 from build_cluster import sanitize, gen_features
 import numpy as np
 import json
 from tweet_cluster_naive import categorize
+from datetime import datetime, date, timedelta
+import os
+
 
 class EventDetect:
 	def __init__(self, threshold):
@@ -48,13 +51,13 @@ class EventDetect:
 			if key == c[0]:
 				self.tweets[cat][c[0]][0].append(tweet)
 				if len(self.tweets[cat][c[0]][0]) >= self.threshold:
-					self.new_event(c[0], self.tweets[cat][c[0]])
+					self.new_event(c[0], self.tweets[cat][c[0]], cat)
 					self.tweets[cat][c[0]][2] = 1
 			else:
 				t_list = self.tweets[cat][c[0]][0]
 				t_list.append(tweet)
 				if len(t_list) >= self.threshold:
-					self.new_event(c[0], self.tweets[cat][c[0]])
+					self.new_event(c[0], self.tweets[cat][c[0]], cat)
 					self.tweets[cat][c[0]][2] = 1
 				self.tweets[cat][key] = [t_list, self.tweets[cat][c[0]][1], self.tweets[cat][c[0]][2]]
 				del self.tweets[cat][c[0]]
@@ -68,55 +71,63 @@ class EventDetect:
 				if tweets[c][t][1] >= time:
 					new_tweets[c][t] = tweets[c][t]
 		self.tweets = new_tweets
-	def new_event(self, k, t):
+	def new_event(self, k, t, c):
+		print("Is it new?")
+		print(t[2])
 		if not t[2]:
-			line = t[0][0] + "\t" + str(t[1]) + "\n"
-			with open("events.txt", "a+") as out:
+			line = t[0][0] + "\t" + str(t[1]) + "\t" + str(c) + "\t0.75\n"
+			with open("events.txt", "a+", os.O_NONBLOCK) as out:
 				out.write(line)
-			#print(self.tweets[k])
+			print(line)
 
 if __name__ == '__main__':
-	ed = EventDetect(5)
+	ed = EventDetect(3)
+	dialation = 0.01
 	#insert()
 	count = 1
 	km = joblib.load("cluster_model.pkl")
 	x = np.array([5, 5, 0, 15, 6]).reshape(1, -1)
 	relevant = km.predict(x)[0]
-	while(True):
-		with open("sandy_2.txt") as f:
-			line = 1
-			line = f.readline().rstrip()
-			while(line):
-				line = tweet_to_json(line)
-				try:
-					t = json.loads(line)
-					if t['text']:
-						text = t['text'].replace("\n", " ")
-						words, tags = sanitize(text)
-						if t['from_user_name'] != 0:
-							line = (words, tags, text, t['from_user_name'])
-						else:
-							line = (words, tags, text, "json issues")
-				except ValueError as e:
-					line = f.readline().rstrip()
-					continue
-				x = np.array(gen_features(line[0])).reshape(1, -1)
-				r = km.predict(x)[0]
-				if r != relevant:
-					line = f.readline().rstrip()
-					continue
-				c = categorize(line[0])
-				if c == 5:
-					line = f.readline().rstrip()
-					continue
-				words = frozenset(line[0])
-				t = [words, line[2], c]
-				ed.insert(t)
-				count += 1
+	with open("3hour.txt") as f:
+		line = 1
+		line = f.readline().rstrip()
+		tweet_time_s = line.split("created_at")[1][9:29]
+		current_time = datetime.strptime(tweet_time_s, '%d %b %Y %H:%M:%S')
+		while(line):
+			tweet_time_s = line.split("created_at")[1][9:29]
+			tweet_time = datetime.strptime(tweet_time_s, '%d %b %Y %H:%M:%S')
+			print(tweet_time, current_time)
+			if tweet_time > current_time:
+				sleep(dialation)
+				current_time += timedelta(seconds=1)
+			line = tweet_to_json(line)
+			try:
+				t = json.loads(line)
+				if t['text']:
+					text = t['text'].replace("\n", " ")
+					words, tags = sanitize(text)
+					if t['from_user_name'] != 0:
+						line = (words, tags, text, t['from_user_name'])
+					else:
+						line = (words, tags, text, "json issues")
+			except ValueError as e:
 				line = f.readline().rstrip()
-				print(count)
-		print("WAIT")
-		time.sleep(5)
+				continue
+			x = np.array(gen_features(line[0])).reshape(1, -1)
+			r = km.predict(x)[0]
+			if r != relevant:
+				line = f.readline().rstrip()
+				continue
+			c = categorize(line[0])
+			if c == 5:
+				line = f.readline().rstrip()
+				continue
+			words = frozenset(line[0])
+			t = [words, line[2], c]
+			ed.insert(t)
+			count += 1
+			line = f.readline().rstrip()
+			print(count)
 
 	"""print(len(ed.tweets))
 	for i in ed.tweets:
